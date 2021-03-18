b53bede28a57c4d12a09284674482bb6ce630bc1
hnnick <hnnickdonne@126.com>
2021-03-18 17:25:13 +0800

    okoko

--- account/forms.py
+++ account/forms.py
@@ -1,7 +1,23 @@
 from django import forms
+from django.contrib.auth.models import User
 
 
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
     age = forms.IntegerField()
+
+
+class RegistrationForm(forms.ModelForm):
+    password = forms.CharField(label="Password", widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Confirm Password", widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ("username", "email") #注明需作用的属性和字段
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError("passwords do not match")
+        return cd['password2']
--- account/urls.py
+++ account/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     #url(r'^login/$', views.user_login, name="user_login"),
     url(r'^login/$', auth_views.LoginView.as_view(template_name='registration/login.html'), name="user_login"),
     url(r'^new/$', auth_views.LoginView.as_view(template_name='account/login.html'), name="user_login"),
-    url(r'^logout/$', auth_views.LogoutView.as_view(), name="user_logout")
+    url(r'^logout/$', auth_views.LogoutView.as_view(), name="user_logout"),
+    url(r'^register/$', views.register, name="user_register")
 ]
