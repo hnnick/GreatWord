31687291f06f4fe72e1804e1fcd30cf898039d3b
hnnick <hnnickonne@126.com>
2021-03-21 19:46:03 +0800

    20210321

--- account/urls.py
+++ account/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
                                                                      success_url=reverse_lazy(
                                                                          'account:password_change_done')),
         name='password_change'),
-    url(r'^password-change/$', auth_views.PasswordChangeDoneView.as_view(template_name=
-                                                                         'registration/password_change_done.html'),
-        name='password_change'),
+    url(r'^password-change-done/$', auth_views.PasswordChangeDoneView.as_view(template_name=
+                                                                              'registration/password_change_done.html'),
+        name='password_change_done'),
 ]
