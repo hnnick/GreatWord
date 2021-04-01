57cfb9fbe51640ef3effd1529c1ca5419931b85e
hnnick <hnnickdonne@126.com>
2021-04-01 17:28:04 +0800

    20210401

--- account/urls.py
+++ account/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     url(r'^password-change-done/$', auth_views.PasswordChangeDoneView.as_view(template_name=
                                                                               'registration/password_change_done.html'),
         name='password_change_done'),
+    url(r'^my-information/$', views.myself, name="my_information"),
 ]
