59ec661db46bb08da75d6ffcd5b8b9391bfac3c1
hnnick <hnnickdonne@126.com>
2021-04-12 18:12:00 +0800

    20210410 下班前上传

--- account/admin.py
+++ account/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import UserProfile
+from .models import UserProfile, UserInfo
 
 
 # Register your models here.
@@ -11,3 +11,11 @@ class UserProfileAdmin(admin.ModelAdmin):
 
 
 admin.site.register(UserProfile, UserProfileAdmin)
+
+
+class UserInfoAdmin(admin.ModelAdmin):
+    list_display = ("user", "school", "company", "profession", "address", "aboutme", "photo")
+    list_filter = ("school", "company", "profession", "aboutme")
+
+
+admin.site.register(UserInfo, UserInfoAdmin)
