e529600f2f1b2c52297dd09b48f266b0c146da41
hnnick <hnnickdonne@126.com>
2021-04-14 17:31:43 +0800

    20210414 下班前上传

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 
 
 from django.conf.urls import url
+from django.urls import include
+
 from . import views
 
 app_name = 'blog'
@@ -8,5 +10,6 @@ urlpatterns = [
     url(r'^$', views.blog_title, name="blog_title"),
     #url(r'( ?P<article_id>\d)$', views.blog_article, name="blog_article"),
     url(r'(?P<article_id>\d)/$', views.blog_article, name="blog_detail"),
+    url(r'^article/', include(('article.urls', "article"), namespace='article')),
 
 ]
